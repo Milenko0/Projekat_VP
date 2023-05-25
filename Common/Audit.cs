05e58c56ddc5622f0d6adc8167df453995b662ab
Milenko0 <milenko.stankovic01@gmail.com>
2023-05-25 17:20:42 +0200

    Add project files.

---
+++ Common/Audit.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [DataContract]
+    public enum MessageType { [EnumMember] Info, [EnumMember] Warning, [EnumMember] Error }
+
+    [DataContract]
+    public class Audit
+    {
+        private int id;
+        private DateTime timestamp;
+        private MessageType messageType;
+        private string message;
+
+        public Audit()
+        {
+
+        }
+
+        public Audit(int id, DateTime timestamp, MessageType messageType, string message)
+        {
+            this.id = id;
+            this.timestamp = timestamp;
+            this.messageType = messageType;
+            this.message = message;
+        }
+
+        [DataMember]
+        public int Id { get => id; set => id = value; }
+        [DataMember]
+        public DateTime Timestamp { get => timestamp; set => timestamp = value; }
+        [DataMember]
+        public MessageType MessageType { get => messageType; set => messageType = value; }
+        [DataMember]
+        public string Message { get => message; set => message = value; }
+
+        public override string ToString()
+        {
+            return Id + " " + Timestamp + " " + MessageType + " " + Message;
+        }
+
+    }
+}
---
+++ Common/Load.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [DataContract]
+    public class Load
+    {
+        private int id;
+        private DateTime timestamp;
+        private double measuredValue;
+
+        public Load()
+        {
+
+        }
+
+        public Load(int id, DateTime timestamp, double measuredValue)
+        {
+            this.id = id;
+            this.timestamp = timestamp;
+            this.measuredValue = measuredValue;
+        }
+
+        [DataMember]
+        public int Id { get => id; set => id = value; }
+        [DataMember]
+        public DateTime Timestamp { get => timestamp; set => timestamp = value; }
+        [DataMember]
+        public double MeasuredValue { get => measuredValue; set => measuredValue = value; }
+
+        public override string ToString()
+        {
+            return Id + " " + Timestamp + " " + MeasuredValue;
+        }
+    }
+}
