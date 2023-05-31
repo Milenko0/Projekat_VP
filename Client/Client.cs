358d553695b81a63f28eaeef2f9b16ad767fbab5
Milenko0 <116080511+Milenko0@users.noreply.github.com>
2023-05-31 12:13:03 +0200

    Dispose pattern

--- Client/Client.cs
+++ Client/Client.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Client
 {
     public class Client : IDisposable
     {
         private ClientServiceProxy serviceProxy;
+        private bool disposedValue = false;
+        private static MemoryStream stream = new MemoryStream();
+        
         public Client()
         {
             serviceProxy = new ClientServiceProxy();
@@ -58,13 +62,43 @@ namespace Client
             }
         }
 
-        public void Dispose()
+        /*public void Dispose()
         {
             if (serviceProxy != null)
             {
                 serviceProxy.Close();
                 serviceProxy = null;
             }
+        }*/
+        
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                    stream.Dispose();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        ~Client()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
 
     }
