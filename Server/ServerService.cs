df84eea12d121268354f3c8978941d70ee1087b6
Milenko0 <116080511+Milenko0@users.noreply.github.com>
2023-05-31 12:15:53 +0200

    Dispose pattern

--- Server/ServerService.cs
+++ Server/ServerService.cs
@@ -10,10 +10,12 @@ using System.Threading.Tasks;
 
 namespace Server
 {
-    public class ServerService : IServerService
+    public class ServerService : IServerService, IDisposable
     {
         private string csvFolderPath;
         private XmlDatabase<Load> xmlDatabase;
+        private bool disposedValue = false;
+        private static MemoryStream stream = new MemoryStream();
 
         public ServerService()
         {
@@ -253,9 +255,41 @@ namespace Server
             return standardDeviation;
         }
 
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                    stream.Dispose();
+
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        ~ServerService()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: false);
+        }
+
         public void Dispose()
         {
-            xmlDatabase.Dispose();
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
+
+        /*public void Dispose()
+        {
+            xmlDatabase.Dispose();
+            //((IDisposable).xmlDatabase).Dispose();
+        }*/
     }
 }
